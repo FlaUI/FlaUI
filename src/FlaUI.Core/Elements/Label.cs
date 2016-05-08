1fa50ce00b4c1060d79a7df0f65f0f7474c23c8f
Max Young <maxdiplomat@gmail.com>
2016-05-08 18:53:40 -0400

    Adds label element with text property

---
+++ src/FlaUI.Core/Elements/Label.cs
@@ -0,0 +1,16 @@
+﻿using interop.UIAutomationCore;
+
+namespace FlaUI.Core.Elements
+{
+    public class Label : AutomationElement
+    {
+        public Label(Automation automation, IUIAutomationElement nativeElement) : base(automation, nativeElement)
+        {
+        }
+
+        public string Text()
+        {
+            return Current.Name;
+        }
+    }
+}
