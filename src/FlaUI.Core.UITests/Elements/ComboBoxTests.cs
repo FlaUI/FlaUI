db448dbf55001e0290849af62886b5e2322452f6
Roemer <r.baeriswyl@gmail.com>
2016-05-09 21:02:31 +0200

    Added missing file

---
+++ src/FlaUI.Core.UITests/Elements/ComboBoxTests.cs
@@ -0,0 +1,26 @@
+﻿using FlaUI.Core.Conditions;
+using FlaUI.Core.Definitions;
+using FlaUI.Core.Input;
+using FlaUI.Core.UITests.TestFramework;
+using NUnit.Framework;
+
+namespace FlaUI.Core.UITests.Elements
+{
+    [TestFixture(TestApplicationType.WinForms)]
+    public class ComboBoxTests : UITestBase
+    {
+        public ComboBoxTests(TestApplicationType appType)
+            : base(appType)
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            var combo = App.GetMainWindow().FindFirst(TreeScope.Descendants, ConditionFactory.ByAutomationId("comboBox1"));
+            combo.Automation.Mouse.Click(MouseButton.Left, combo.Current.BoundingRectangle.ImmediateInteriorEast);
+            var items = combo.FindAll(TreeScope.Descendants, new BoolCondition(true));
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
