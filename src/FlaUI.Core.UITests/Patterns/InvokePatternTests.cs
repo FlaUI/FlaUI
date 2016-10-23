2f169566d0803070bf3eabaabc81615bf021d665
Roemer <r.baeriswyl@gmail.com>
2016-10-23 21:16:48 +0200

    Added InvokePattern tests

---
+++ src/FlaUI.Core.UITests/Patterns/InvokePatternTests.cs
@@ -0,0 +1,39 @@
+﻿using FlaUI.Core.AutomationElements.Infrastructure;
+using FlaUI.Core.Definitions;
+using FlaUI.Core.UITests.TestFramework;
+using NUnit.Framework;
+
+namespace FlaUI.Core.UITests.Patterns
+{
+    [TestFixture(AutomationType.UIA2, TestApplicationType.Wpf)]
+    [TestFixture(AutomationType.UIA3, TestApplicationType.Wpf)]
+    public class InvokePatternTests : UITestBase
+    {
+        public InvokePatternTests(AutomationType automationType, TestApplicationType appType)
+            : base(automationType, appType)
+        {
+        }
+
+        [Test]
+        public void InvokeWithEventTest()
+        {
+            var mainWindow = App.GetMainWindow(Automation);
+            var tab = mainWindow.FindFirstDescendant(mainWindow.ConditionFactory.ByControlType(ControlType.Tab)).AsTab();
+            var tabItem = tab.TabItems[0];
+            var button = tabItem.FindFirstDescendant(mainWindow.ConditionFactory.ByAutomationId("InvokableButton"));
+            Assert.That(button, Is.Not.Null);
+            var origButtonText = button.Current.Name;
+            var invokePattern = button.PatternFactory.GetInvokePattern();
+            Assert.That(invokePattern, Is.Not.Null);
+            var invokeFired = false;
+            var registeredEvent = button.RegisterEvent(invokePattern.Events.InvokedEvent, TreeScope.Element, (element, id) =>
+            {
+                invokeFired = true;
+            });
+            invokePattern.Invoke();
+            Assert.That(button.Current.Name, Is.Not.EqualTo(origButtonText));
+            Assert.That(invokeFired, Is.True);
+            button.RemoveAutomationEventHandler(invokePattern.Events.InvokedEvent, registeredEvent);
+        }
+    }
+}
--- src/TestApplications/WpfApplication/MainViewModel.cs
+++ src/TestApplications/WpfApplication/MainViewModel.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using WpfApplication.Infrastructure;
 
 namespace WpfApplication
 {
-    public class MainViewModel
+    public class MainViewModel : ObservableObject
     {
         public ObservableCollection<DataGridItem> DataGridItems { get; }
+
+        public ICommand InvokeButtonCommand { get; }
+
+        public string InvokeButtonText { get { return GetProperty<string>(); } set { SetProperty(value); } }
 
         public MainViewModel()
         {
@@ -12,6 +18,9 @@ namespace WpfApplication
             DataGridItems.Add(new DataGridItem { Id = 1, Name = "Spongebob" });
             DataGridItems.Add(new DataGridItem { Id = 2, Name = "Patrick" });
             DataGridItems.Add(new DataGridItem { Id = 3, Name = "Tadeus" });
+
+            InvokeButtonText = "Invoke me!";
+            InvokeButtonCommand = new RelayCommand(o => InvokeButtonText = "Invoked!");
         }
     }
 }
