84e3644249f5e3cf4e3b1bab13221d194f3cb0d3
Roemer <r.baeriswyl@gmail.com>
2017-02-25 14:52:32 +0100

    Migrated Drag Pattern

--- src/FlaUI.Core/Patterns/DragPattern.cs
+++ src/FlaUI.Core/Patterns/DragPattern.cs
@@ -8,10 +8,11 @@ namespace FlaUI.Core.Patterns
     {
         IDragPatternProperties Properties { get; }
         IDragPatternEvents Events { get; }
-        string DropEffect { get; }
-        string[] DropEffects { get; }
-        bool IsGrabbed { get; }
-        AutomationElement[] GrabbedItems { get; }
+
+        AutomationProperty<string> DropEffect { get; }
+        AutomationProperty<string[]> DropEffects { get; }
+        AutomationProperty<bool> IsGrabbed { get; }
+        AutomationProperty<AutomationElement[]> GrabbedItems { get; }
     }
 
     public interface IDragPatternProperties
@@ -27,5 +28,24 @@ namespace FlaUI.Core.Patterns
         EventId DragCancelEvent { get; }
         EventId DragCompleteEvent { get; }
         EventId DragStartEvent { get; }
+    }
+
+    public abstract class DragPatternBase<TNativePattern> : PatternBase<TNativePattern>, IDragPattern
+    {
+        protected DragPatternBase(BasicAutomationElementBase basicAutomationElement, TNativePattern nativePattern) : base(basicAutomationElement, nativePattern)
+        {
+            DropEffect = new AutomationProperty<string>(() => Properties.DropEffectProperty, BasicAutomationElement);
+            DropEffects = new AutomationProperty<string[]>(() => Properties.DropEffectsProperty, BasicAutomationElement);
+            IsGrabbed = new AutomationProperty<bool>(() => Properties.IsGrabbedProperty, BasicAutomationElement);
+            GrabbedItems = new AutomationProperty<AutomationElement[]>(() => Properties.GrabbedItemsProperty, BasicAutomationElement);
+        }
+
+        public IDragPatternProperties Properties => Automation.PropertyLibrary.Drag;
+        public IDragPatternEvents Events => Automation.EventLibrary.Drag;
+
+        public AutomationProperty<string> DropEffect { get; }
+        public AutomationProperty<string[]> DropEffects { get; }
+        public AutomationProperty<bool> IsGrabbed { get; }
+        public AutomationProperty<AutomationElement[]> GrabbedItems { get; }
     }
 }
--- src/FlaUI.UIA3/Patterns/DragPattern.cs
+++ src/FlaUI.UIA3/Patterns/DragPattern.cs
@@ -1,46 +1,25 @@
 ﻿using FlaUI.Core;
-using FlaUI.Core.AutomationElements.Infrastructure;
 using FlaUI.Core.Identifiers;
 using FlaUI.Core.Patterns;
-using FlaUI.Core.Patterns.Infrastructure;
 using FlaUI.UIA3.Converters;
 using FlaUI.UIA3.Identifiers;
 using UIA = interop.UIAutomationCore;
 
 namespace FlaUI.UIA3.Patterns
 {
-    public class DragPattern : PatternBase<UIA.IUIAutomationDragPattern>, IDragPattern
+    public class DragPattern : DragPatternBase<UIA.IUIAutomationDragPattern>
     {
         public static readonly PatternId Pattern = PatternId.Register(AutomationType.UIA3, UIA.UIA_PatternIds.UIA_DragPatternId, "Drag", AutomationObjectIds.IsDragPatternAvailableProperty);
         public static readonly PropertyId DropEffectProperty = PropertyId.Register(AutomationType.UIA3, UIA.UIA_PropertyIds.UIA_DragDropEffectPropertyId, "DropEffect");
         public static readonly PropertyId DropEffectsProperty = PropertyId.Register(AutomationType.UIA3, UIA.UIA_PropertyIds.UIA_DragDropEffectsPropertyId, "DropEffects");
         public static readonly PropertyId IsGrabbedProperty = PropertyId.Register(AutomationType.UIA3, UIA.UIA_PropertyIds.UIA_DragIsGrabbedPropertyId, "IsGrabbed");
-        public static readonly PropertyId GrabbedItemsProperty = PropertyId.Register(AutomationType.UIA3, UIA.UIA_PropertyIds.UIA_DragGrabbedItemsPropertyId, " GrabbedItems");
+        public static readonly PropertyId GrabbedItemsProperty = PropertyId.Register(AutomationType.UIA3, UIA.UIA_PropertyIds.UIA_DragGrabbedItemsPropertyId, " GrabbedItems").SetConverter(AutomationElementConverter.NativeArrayToManaged);
         public static readonly EventId DragCancelEvent = EventId.Register(AutomationType.UIA3, UIA.UIA_EventIds.UIA_Drag_DragCancelEventId, "DragCancel");
         public static readonly EventId DragCompleteEvent = EventId.Register(AutomationType.UIA3, UIA.UIA_EventIds.UIA_Drag_DragCompleteEventId, "DragComplete");
         public static readonly EventId DragStartEvent = EventId.Register(AutomationType.UIA3, UIA.UIA_EventIds.UIA_Drag_DragStartEventId, "DragStart");
 
         public DragPattern(BasicAutomationElementBase basicAutomationElement, UIA.IUIAutomationDragPattern nativePattern) : base(basicAutomationElement, nativePattern)
         {
-        }
-        
-        public IDragPatternProperties Properties => Automation.PropertyLibrary.Drag;
-
-        public IDragPatternEvents Events => Automation.EventLibrary.Drag;
-
-        public string DropEffect => Get<string>(DropEffectProperty);
-
-        public string[] DropEffects => Get<string[]>(DropEffectsProperty);
-
-        public bool IsGrabbed => Get<bool>(IsGrabbedProperty);
-
-        public AutomationElement[] GrabbedItems
-        {
-            get
-            {
-                var nativeElement = Get<UIA.IUIAutomationElementArray>(GrabbedItemsProperty);
-                return AutomationElementConverter.NativeArrayToManaged((UIA3Automation)BasicAutomationElement.Automation, nativeElement);
-            }
         }
     }
 
