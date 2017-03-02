3a1ea95428b1a39be14e333d5b7d9b73f9e05d0f
Roemer <r.baeriswyl@gmail.com>
2017-03-02 20:25:22 +0100

    Fixed output of tests

--- RunTests.bat
+++ RunTests.bat
@@ -1,4 +1,4 @@
 SET config=Release
-nunit3-console "src\FlaUI.Core.UnitTests\FlaUI.Core.UnitTests.csproj" --config=%config% --output=UnitTestResult.xml
-nunit3-console "src\FlaUI.Core.UITests\FlaUI.Core.UITests.csproj" --config=%config% --params=uia=2 --output=UIA2TestResult.xml
-nunit3-console "src\FlaUI.Core.UITests\FlaUI.Core.UITests.csproj" --config=%config% --params=uia=3 --output=UIA3TestResult.xml
+nunit3-console "src\FlaUI.Core.UnitTests\FlaUI.Core.UnitTests.csproj" --config=%config% --result=UnitTestResult.xml
+nunit3-console "src\FlaUI.Core.UITests\FlaUI.Core.UITests.csproj" --config=%config% --params=uia=2 --result=UIA2TestResult.xml
+nunit3-console "src\FlaUI.Core.UITests\FlaUI.Core.UITests.csproj" --config=%config% --params=uia=3 --result=UIA3TestResult.xml
