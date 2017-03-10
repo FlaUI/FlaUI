556173bac3b84c13e6644aaa402f4b1817399b67
Roemer <r.baeriswyl@gmail.com>
2017-03-11 00:17:36 +0100

    Fixed path

--- RunTests.bat
+++ RunTests.bat
@@ -1,9 +1,9 @@
 SET config=Release
 SET /A errno=0
-n\nunit3-console "src\FlaUI.Core.UnitTests\FlaUI.Core.UnitTests.csproj" --config=%config% --result=UnitTestResult.xml
+nunit3-console "src\FlaUI.Core.UnitTests\FlaUI.Core.UnitTests.csproj" --config=%config% --result=UnitTestResult.xml
 SET /A errno=%errno%+%ERRORLEVEL%
-n\nunit3-console "src\FlaUI.Core.UITests\FlaUI.Core.UITests.csproj" --config=%config% --params=uia=2 --result=UIA2TestResult.xml
+nunit3-console "src\FlaUI.Core.UITests\FlaUI.Core.UITests.csproj" --config=%config% --params=uia=2 --result=UIA2TestResult.xml
 SET /A errno=%errno%+%ERRORLEVEL%
-n\nunit3-console "src\FlaUI.Core.UITests\FlaUI.Core.UITests.csproj" --config=%config% --params=uia=3 --result=UIA3TestResult.xml
+nunit3-console "src\FlaUI.Core.UITests\FlaUI.Core.UITests.csproj" --config=%config% --params=uia=3 --result=UIA3TestResult.xml
 SET /A errno=%errno%+%ERRORLEVEL%
 EXIT /B %errno%
