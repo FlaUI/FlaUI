f4f51b09a47bbbb0bb789f9f806e62b3a49da568
Roemer <r.baeriswyl@gmail.com>
2017-03-14 16:55:50 +0100

    Fixed install script

--- nuspec/install.ps1
+++ nuspec/install.ps1
@@ -1,4 +1,4 @@
 param($installPath, $toolsPath, $package, $project)
 
-$project.Object.References | Where-Object { $_.EmbedInteropTypes -eq $true -and $_.Name -eq "interop.UIAutomationCore" } |  ForEach-Object { $_.EmbedInteropTypes = $false }
+$project.Object.References | Where-Object { $_.EmbedInteropTypes -eq $true -and $_.Name -eq "Interop.UIAutomationClient" } |  ForEach-Object { $_.EmbedInteropTypes = $false }
 $project.Save()
