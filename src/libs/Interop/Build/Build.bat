80071134013ede32e566994546a0cb7bd640cf4d
Bäriswyl Roman <roman.baeriswyl@elca.ch>
2017-08-22 17:32:51 +0200

    Added signed interops

--- src/libs/Interop/Build/Build.bat
+++ src/libs/Interop/Build/Build.bat
@@ -9,18 +9,26 @@ mkdir %TEMP%
 @REM Create Type Libraries
 midl.exe /nologo /out %TEMP% /char signed /tlb UIAutomationClient.tlb /h UIAutomationClient_h.h "%WINSDK%UIAutomationClient.idl"
 midl.exe /nologo /out %TEMP% /char signed /tlb UIAutomationCore.tlb /h UIAutomationCore_h.h "%WINSDK%UIAutomationCore.idl"
-http://stackoverflow.com/questions/5615206/windows-batch-files-setting-variable-in-for-loop
+@REM http://stackoverflow.com/questions/5615206/windows-batch-files-setting-variable-in-for-loop
 FOR %%A IN (3.5 4.5) DO (
 	mkdir %TEMP%\%%A
+	mkdir %TEMP%\%%A\Signed
+
 	@REM Create the original dlls from the tlbs
-	%%A\tlbimp.exe /machine:Agnostic /silent /asmversion:%%A /out:%%A\Interop.UIAutomationClient.dll %TEMP%\UIAutomationClient.tlb
-	%%A\tlbimp.exe /machine:Agnostic /silent /asmversion:%%A /out:%TEMP%\%%A\Interop.UIAutomationCore.dll %TEMP%\UIAutomationCore.tlb
-	REM %%A\tlbimp.exe /machine:Agnostic /silent /asmversion:%%A /out:%TEMP%\%%A\Interop.UIAutomationClient.dll %TEMP%\UIAutomationClient.tlb /keyfile:..\..\FlaUI.snk"
-	REM %%A\tlbimp.exe /machine:Agnostic /silent /asmversion:%%A /out:%TEMP%\%%A\Interop.UIAutomationCore.dll %TEMP%\UIAutomationCore.tlb /keyfile:..\..\FlaUI.snk"
+	REM %%A\tlbimp.exe /machine:Agnostic /silent /asmversion:%%A /out:%%A\Interop.UIAutomationClient.dll %TEMP%\UIAutomationClient.tlb
+	REM %%A\tlbimp.exe /machine:Agnostic /silent /asmversion:%%A /out:%TEMP%\%%A\Interop.UIAutomationCore.dll %TEMP%\UIAutomationCore.tlb
+
+	%%A\tlbimp.exe /machine:Agnostic /silent /asmversion:%%A /out:%%A\Signed\Interop.UIAutomationClient.dll %TEMP%\UIAutomationClient.tlb /keyfile:..\..\..\..\FlaUI.snk"
+	%%A\tlbimp.exe /machine:Agnostic /silent /asmversion:%%A /out:%TEMP%\%%A\Signed\Interop.UIAutomationCore.dll %TEMP%\UIAutomationCore.tlb /keyfile:..\..\..\..\FlaUI.snk"
+
 	@REM Fix Core by IL Code
-	ildasm.exe %TEMP%\%%A\Interop.UIAutomationCore.dll /out=%TEMP%\%%A\UIAutomationCore.il /nobar
-	powershell -File "FixAutomationCore.ps1" "%TEMP%\%%A\UIAutomationCore.il"
-	ilasm /dll /output=%%A\Interop.UIAutomationCore.dll %TEMP%\%%A\UIAutomationCore.il /res:%TEMP%\%%A\UIAutomationCore.res
+	REM ildasm.exe %TEMP%\%%A\Interop.UIAutomationCore.dll /out=%TEMP%\%%A\UIAutomationCore.il /nobar
+	REM powershell -File "FixAutomationCore.ps1" "%TEMP%\%%A\UIAutomationCore.il"
+	REM ilasm /dll /output=%%A\Interop.UIAutomationCore.dll %TEMP%\%%A\UIAutomationCore.il /res:%TEMP%\%%A\UIAutomationCore.res
+
+	ildasm.exe %TEMP%\%%A\Signed\Interop.UIAutomationCore.dll /out=%TEMP%\%%A\Signed\UIAutomationCore.il /nobar
+	powershell -File "FixAutomationCore.ps1" "%TEMP%\%%A\Signed\UIAutomationCore.il"
+	ilasm /dll /output=%%A\Signed\Interop.UIAutomationCore.dll %TEMP%\%%A\Signed\UIAutomationCore.il /res:%TEMP%\%%A\Signed\UIAutomationCore.res
 )
 RMDIR /S /Q %TEMP%
 pause
