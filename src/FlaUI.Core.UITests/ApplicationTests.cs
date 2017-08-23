4389e8590705ce33d4149020f2623414c1377b41
Bäriswyl Roman <roman.baeriswyl@elca.ch>
2017-08-23 18:01:51 +0200

    Added application test

---
+++ src/FlaUI.Core.UITests/ApplicationTests.cs
@@ -0,0 +1,21 @@
+﻿using FlaUI.UIA3;
+using NUnit.Framework;
+
+namespace FlaUI.Core.UITests
+{
+    [TestFixture]
+    public class ApplicationTests
+    {
+        [Test]
+        public void DisposeWhenClosed()
+        {
+            using (var automation = new UIA3Automation())
+            {
+                using (var app = Application.Launch("notepad.exe"))
+                {
+                    app.Close();
+                }
+            }
+        }
+    }
+}
