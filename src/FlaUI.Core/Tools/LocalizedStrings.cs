eb85e81294cf09753504ddfd5b9e2f9fba16f85f
Bäriswyl Roman <roman.baeriswyl@elca.ch>
2017-10-06 10:17:48 +0200

    Added some more localized strings

--- src/FlaUI.Core/Tools/LocalizedStrings.cs
+++ src/FlaUI.Core/Tools/LocalizedStrings.cs
@@ -10,6 +10,10 @@
             switch (OperatingSystem.CurrentCulture.TwoLetterISOLanguageName)
             {
                 case "de":
+                    WinFormsUIA2VerticalScrollBarName = "Vertikale Bildlaufleiste";
+                    WinFormsUIA3VerticalScrollBarName = "Vertikale Bildlaufleiste";
+                    WinFormsUIA2HorizontalScrollBarName = "Horizontale Bildlaufleiste";
+                    WinFormsUIA3HorizontalScrollBarName = "Horizontale Bildlaufleiste";
                     HorizontalScrollBar = "Horizontale Schiebeleiste";
                     VerticalScrollBar = "Vertikale Schiebeleiste";
                     TableHorizontalScrollBar = "Horizontale Schiebeleiste";
@@ -18,8 +22,12 @@
                     DataGridViewHeaderItemTopLeft = "Obere linke Headerzelle";
                     break;
                 default:
+                    WinFormsUIA2VerticalScrollBarName = "Vertical Scroll Bar";
+                    WinFormsUIA3VerticalScrollBarName = "Vertical";
+                    WinFormsUIA2HorizontalScrollBarName = "Horizontal Scroll Bar";
+                    WinFormsUIA3HorizontalScrollBarName = "Horizontal";
                     HorizontalScrollBar = "Horizontal ScrollBar";
-                    VerticalScrollBar = "Vertical ScrollBar";
+                    VerticalScrollBar = "Vertical ScrollBar";                
                     TableHorizontalScrollBar = "Horizontal Scroll Bar";
                     TableVerticalScrollBar = "Vertical Scroll Bar";
                     DataGridViewHeader = "Top Row";
@@ -29,9 +37,32 @@
         }
 
         public static string HorizontalScrollBar { get; }
+
         public static string VerticalScrollBar { get; }
+
         public static string TableHorizontalScrollBar { get; }
+
         public static string TableVerticalScrollBar { get; }
+
+        /// <summary>
+        /// Name of a WinForms vertical scrollbar in UIA2.
+        /// </summary>
+        public static string WinFormsUIA2VerticalScrollBarName { get; }
+
+        /// <summary>
+        /// Name of a WinForms vertical scrollbar in UIA3.
+        /// </summary>
+        public static string WinFormsUIA3VerticalScrollBarName { get; }
+
+        /// <summary>
+        /// Name of a WinForms horizontal scrollbar in UIA2.
+        /// </summary>
+        public static string WinFormsUIA2HorizontalScrollBarName { get; }
+
+        /// <summary>
+        /// Name of a WinForms horizontal scrollbar in UIA3.
+        /// </summary>
+        public static string WinFormsUIA3HorizontalScrollBarName { get; }
 
         /// <summary>
         /// Name of the header row in a DataGridView.
