b7c4dc36684e41ec77ee162a10e703c96ff7f882
Roman Baeriswyl <r.baeriswyl@gmail.com>
2018-01-16 14:53:56 +0100

    Stub for UIA3TextRange3

---
+++ src/FlaUI.Core/ITextRange3.cs
@@ -0,0 +1,12 @@
+﻿using FlaUI.Core.AutomationElements.Infrastructure;
+using FlaUI.Core.Identifiers;
+
+namespace FlaUI.Core
+{
+    public interface ITextRange3 : ITextRange2
+    {
+        AutomationElement GetEnclosingElementBuildCache();
+        AutomationElement[] GetChildrenBuildCache();
+        object[] GetAttributeValues(TextAttributeId[] attributeIds);
+    }
+}
---
+++ src/FlaUI.UIA3/UIA3TextRange3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FlaUI.Core;
+using FlaUI.Core.AutomationElements.Infrastructure;
+using FlaUI.Core.Identifiers;
+using UIA = Interop.UIAutomationClient;
+
+namespace FlaUI.UIA3
+{
+    public class UIA3TextRange3 : UIA3TextRange2, ITextRange3
+    {
+        public UIA.IUIAutomationTextRange3 NativeRange3 { get; }
+
+        public UIA3TextRange3(UIA3Automation automation, UIA.IUIAutomationTextRange3 nativeRange)
+            : base(automation, nativeRange)
+        {
+            NativeRange3 = nativeRange;
+        }
+
+        public AutomationElement GetEnclosingElementBuildCache()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AutomationElement[] GetChildrenBuildCache()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] GetAttributeValues(TextAttributeId[] attributeIds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
