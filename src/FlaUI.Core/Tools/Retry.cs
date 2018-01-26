b141674dda4c4919882583487ffb432ca2b2aca4
Roman Baeriswyl <r.baeriswyl@gmail.com>
2018-01-26 16:05:12 +0100

    Reworked Retry

--- src/FlaUI.Core/Tools/Retry.cs
+++ src/FlaUI.Core/Tools/Retry.cs
@@ -3,36 +3,129 @@ using System.Threading;
 
 namespace FlaUI.Core.Tools
 {
+    /// <summary>
+    /// Static class with methods for retrying actions.
+    /// </summary>
     public static class Retry
     {
-        public static readonly TimeSpan DefaultRetryFor = TimeSpan.FromMilliseconds(1000);
-        private static readonly TimeSpan DefaultRetryInterval = TimeSpan.FromMilliseconds(200);
+        /// <summary>
+        /// The default timeout to use for retries without a given timeout. The default is 1000ms.
+        /// </summary>
+        public static TimeSpan Timeout { get; set; } = TimeSpan.FromMilliseconds(1000);
 
-        public static void WhileException(Action retryAction, TimeSpan timeout, TimeSpan? retryInterval = null)
+        /// <summary>
+        /// The default interval to use for retries without a given interval. The default is 100ms.
+        /// </summary>
+        public static TimeSpan Interval { get; set; } = TimeSpan.FromMilliseconds(100);
+
+        /// <summary>
+        /// Retries while the given method evaluates to true.
+        /// </summary>
+        public static void While(Func<bool> checkMethod, TimeSpan? timeout = null, TimeSpan? interval = null, bool throwOnTimeout = false, bool ignoreException = false)
         {
+            timeout = timeout ?? Timeout;
+            interval = interval ?? Interval;
             var startTime = DateTime.UtcNow;
-            while (true)
+            Exception lastException = null;
+            do
             {
                 try
                 {
-                    retryAction();
+                    if (!checkMethod())
+                    {
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (!ignoreException)
+                    {
+                        throw;
+                    }
+                    lastException = ex;
+                }
+                Thread.Sleep(interval.Value);
+            } while (!IsTimeoutReached(startTime, timeout.Value));
+            if (throwOnTimeout)
+            {
+                throw new TimeoutException("Timeout occurred in retry", lastException);
+            }
+        }
+
+        /// <summary>
+        /// Retries while the given method evaluates to true and returns the value from the method.
+        /// </summary>
+        public static T While<T>(Func<T> retryMethod, Func<T, bool> checkMethod, TimeSpan? timeout = null, TimeSpan? interval = null, bool throwOnTimeout = false, bool ignoreException = false)
+        {
+            timeout = timeout ?? Timeout;
+            interval = interval ?? Interval;
+            var startTime = DateTime.UtcNow;
+            Exception lastException = null;
+            do
+            {
+                try
+                {
+                    var obj = retryMethod();
+                    if (!checkMethod(obj))
+                    {
+                        return obj;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (!ignoreException)
+                    {
+                        throw;
+                    }
+                    lastException = ex;
+                }
+                Thread.Sleep(interval.Value);
+            } while (!IsTimeoutReached(startTime, timeout.Value));
+            if (throwOnTimeout)
+            {
+                throw new TimeoutException("Timeout occurred in retry", lastException);
+            }
+            return default(T);
+        }
+
+        /// <summary>
+        /// Retries while the given method has an exception.
+        /// </summary>
+        public static void WhileException(Action retryMethod, TimeSpan? timeout = null, TimeSpan? interval = null, bool throwOnTimeout = false)
+        {
+            timeout = timeout ?? Timeout;
+            interval = interval ?? Interval;
+            var startTime = DateTime.UtcNow;
+            Exception lastException;
+            do
+            {
+                try
+                {
+                    retryMethod();
                     return;
                 }
                 catch (Exception ex)
                 {
-                    if (IsTimeouted(startTime, timeout))
-                    {
-                        throw new Exception("Timeout occurred in retry", ex);
-                    }
-                    Thread.Sleep(retryInterval ?? DefaultRetryInterval);
+                    lastException = ex;
                 }
+                Thread.Sleep(interval.Value);
+            } while (!IsTimeoutReached(startTime, timeout.Value));
+            if (throwOnTimeout)
+            {
+                throw new TimeoutException("Timeout occurred in retry", lastException);
             }
         }
 
-        public static T WhileException<T>(Func<T> retryMethod, TimeSpan timeout, TimeSpan? retryInterval = null)
+        /// <summary>
+        /// Retries while the given method has an exception and returns the value from the method.
+        /// </summary>
+        public static T WhileException<T>(Func<T> retryMethod, TimeSpan? timeout = null, TimeSpan? interval = null, bool throwOnTimeout = false)
         {
+            timeout = timeout ?? Timeout;
+            interval = interval ?? Interval;
             var startTime = DateTime.UtcNow;
-            while (true)
+            Exception lastException;
+            do
             {
                 try
                 {
@@ -40,58 +133,28 @@ namespace FlaUI.Core.Tools
                 }
                 catch (Exception ex)
                 {
-                    if (IsTimeouted(startTime, timeout))
-                    {
-                        throw new Exception("Timeout occurred in retry", ex);
-                    }
-                    Thread.Sleep(retryInterval ?? DefaultRetryInterval);
+                    lastException = ex;
                 }
-            }
-        }
-
-        public static T While<T>(Func<T> retryMethod, Predicate<T> whilePredicate, TimeSpan timeout, TimeSpan? retryInterval = null)
-        {
-            var startTime = DateTime.UtcNow;
-            while (true)
+                Thread.Sleep(interval.Value);
+            } while (!IsTimeoutReached(startTime, timeout.Value));
+            if (throwOnTimeout)
             {
-                var obj = retryMethod();
-                if (!whilePredicate(obj))
-                {
-                    return obj;
-                }
-                if (IsTimeouted(startTime, timeout))
-                {
-                    return obj;
-                }
-                Thread.Sleep(retryInterval ?? DefaultRetryInterval);
+                throw new TimeoutException("Timeout occurred in retry", lastException);
             }
+            return default(T);
         }
 
-        public static void While(Func<bool> whilePredicate, TimeSpan timeout, TimeSpan? retryInterval = null)
-        {
-            var startTime = DateTime.UtcNow;
-            while (true)
-            {
-                if (!whilePredicate())
-                {
-                    return;
-                }
-                if (IsTimeouted(startTime, timeout))
-                {
-                    return;
-                }
-                Thread.Sleep(retryInterval ?? DefaultRetryInterval);
-            }
-        }
-
-        private static bool IsTimeouted(DateTime startTime, TimeSpan timeout)
+        /// <summary>
+        /// Method which checks if the timeout is reached.
+        /// </summary>
+        private static bool IsTimeoutReached(DateTime startTime, TimeSpan timeout)
         {
             // Check for infinite timeout
             if (timeout.TotalMilliseconds < 0)
             {
                 return false;
             }
-            return DateTime.UtcNow.Subtract(startTime) >= timeout;
+            return DateTime.UtcNow.Subtract(startTime) > timeout;
         }
     }
 }
