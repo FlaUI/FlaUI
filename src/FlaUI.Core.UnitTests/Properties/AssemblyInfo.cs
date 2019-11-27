167dcc8fd699e47de72261127c023d64097fd036
Roman Bäriswyl <roman.baeriswyl@elca.ch>
2019-11-27 08:29:37 +0100

    Fixed year in copyright

--- src/FlaUI.Core.UnitTests/Properties/AssemblyInfo.cs
+++ src/FlaUI.Core.UnitTests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FlaUI.Core.UnitTests")]
-[assembly: AssemblyCopyright("Copyright © 2016-2018")]
+[assembly: AssemblyCopyright("Copyright © 2016-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
