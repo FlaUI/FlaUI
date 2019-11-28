c4b1443a4152f42da5f6fbe851ca198641f0b9de
ddeltasolutions <daniel.mocanu072180@gmail.com>
2019-11-28 17:12:54 +0200

    Add messages and constants for Win32 checkbox

--- src/FlaUI.Core/WindowsAPI/Constants.cs
+++ src/FlaUI.Core/WindowsAPI/Constants.cs
@@ -2108,5 +2108,15 @@ namespace FlaUI.Core.WindowsAPI
         public static IntPtr TRUE = new IntPtr(1);
         public static IntPtr FALSE = new IntPtr(0);
     }
+    
+    internal class ButtonMessages
+    {
+        public static UInt32 BM_SETCHECK = 0x00F1;  // set radio or checkbox button state
+        public static UInt32 BM_GETCHECK = 0x00F0;
+
+        public static UInt32 BST_UNCHECKED = 0x0000;
+        public static UInt32 BST_CHECKED = 0x0001;
+        public static UInt32 BST_INDETERMINATE = 0x0002;
+    }
 }
 #pragma warning restore
