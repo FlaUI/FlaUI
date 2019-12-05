72f798b27c010d92cfe8760fcf23960bf8b3f138
Roman Bäriswyl <roman.baeriswyl@elca.ch>
2019-12-05 16:23:24 +0100

    Take a screenshot in WordPadTests if a test fails

--- src/FlaUI.Core.UITests/WordPad/WordPadTests.cs
+++ src/FlaUI.Core.UITests/WordPad/WordPadTests.cs
@@ -1,7 +1,11 @@
-﻿using FlaUI.Core.AutomationElements;
+﻿using System;
+using System.IO;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Capturing;
 using FlaUI.Core.UITests.WordPad.Screens;
 using FlaUI.UIA3;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 
 namespace FlaUI.Core.UITests.WordPad
 {
@@ -23,6 +27,11 @@ namespace FlaUI.Core.UITests.WordPad
         [TearDown]
         public void TestTearDown()
         {
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                TakeScreenshot(TestContext.CurrentContext.Test.FullName);
+            }
+            
             application.Close();
             application.Dispose();
             application = null;
@@ -52,6 +61,24 @@ namespace FlaUI.Core.UITests.WordPad
                 var infoScreen = mainScreen.OpenAndGetInfoScreen();
                 infoScreen.OkButton.Invoke();
             });
+        }
+
+        private void TakeScreenshot(string screenshotName)
+        {
+            const string failedTestsData = @"c:\FailedTestsData";
+
+            var imagename = screenshotName + ".png";
+            imagename = imagename.Replace("\"", String.Empty);
+            var imagePath = Path.Combine(failedTestsData, imagename);
+            try
+            {
+                Directory.CreateDirectory(failedTestsData);
+                Capture.Screen().ToFile(imagePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to save screenshot to directory: {0}, filename: {1}, Ex: {2}", failedTestsData, imagePath, ex);
+            }
         }
     }
 }
