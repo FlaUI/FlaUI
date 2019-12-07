5264a62aa836bc3d363b837076f888deffbb1200
ddeltasolutions <daniel.mocanu072180@gmail.com>
2019-12-07 12:31:23 +0200

    Correct syntax errors

--- src/FlaUI.Core.UITests/Elements/MenuTests.cs
+++ src/FlaUI.Core.UITests/Elements/MenuTests.cs
@@ -78,12 +78,12 @@ namespace FlaUI.Core.UITests.Elements
             Assert.That(edit, Is.Not.Null);
             var showLabel = edit.Items["Show Label"];
             Assert.That(showLabel, Is.Not.Null);
-            showLabel.Checked = false;
+            showLabel.IsChecked = false;
             Assert.That(showLabel.IsChecked, Is.EqualTo(false));
             var label = window.FindFirstDescendant(cf => cf.ByText("Menu Item Checked")).AsLabel();
             Assert.That(label, Is.Not.Null);
             Assert.That(label.Properties.IsOffscreen.Value, Is.EqualTo(true));
-            showLabel.Checked = true;
+            showLabel.IsChecked = true;
             Assert.That(showLabel.IsChecked, Is.EqualTo(true));
             Assert.That(label.Properties.IsOffscreen.Value, Is.EqualTo(false));
         }
