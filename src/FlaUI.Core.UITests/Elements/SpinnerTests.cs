c0e3f74e7c8137063aefdb84268a143d2db27856
ddeltasolutions <daniel.mocanu072180@gmail.com>
2019-12-10 18:07:58 +0200

    Add SpinnerTests.cs file

---
+++ src/FlaUI.Core.UITests/Elements/SpinnerTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Tools;
+using FlaUI.Core.UITests.TestFramework;
+using NUnit.Framework;
+
+namespace FlaUI.Core.UITests.Elements
+{
+    [TestFixture(AutomationType.UIA2, TestApplicationType.WinForms)]
+    [TestFixture(AutomationType.UIA3, TestApplicationType.WinForms)]
+    public class SpinnerTests : UITestBase
+    {
+        public SpinnerTests(AutomationType automationType, TestApplicationType appType)
+            : base(automationType, appType)
+        {
+        }
+
+        [Test]
+        public void SetValueTest()
+        {
+            var spinner = GetSpinner();
+            spinner.Value = 6;
+            Assert.That(spinner.Value, Is.EqualTo(6));
+            spinner.Value = 4;
+            Assert.That(spinner.Value, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void IncrementTest()
+        {
+            var spinner = GetSpinner();
+            spinner.Value = 5;
+            spinner.Increment();
+            Assert.That(spinner.Value, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void DecrementTest()
+        {
+            var spinner = GetSpinner();
+            spinner.Value = 5;
+            spinner.Decrement();
+            Assert.That(spinner.Value, Is.EqualTo(4));
+        }
+
+        private Spinner GetSpinner()
+        {
+            var element = App.GetMainWindow(Automation).FindFirstDescendant(cf => cf.ByAutomationId("numericUpDown1")).AsSpinner();
+            return element;
+        }
+    }
+}
