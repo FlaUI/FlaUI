79a0d16965e55c9bf038f284689f39ad94942674
ddeltasolutions <daniel.mocanu072180@gmail.com>
2020-01-25 17:14:41 +0200

    Add test for AddToSelection method

--- src/FlaUI.Core.UITests/Elements/CalendarTests.cs
+++ src/FlaUI.Core.UITests/Elements/CalendarTests.cs
@@ -12,6 +12,8 @@ namespace FlaUI.Core.UITests.Elements
     [TestFixture(AutomationType.UIA3, TestApplicationType.Wpf)]
     public class CalendarTests : UITestBase
     {
+        Calendar calendar = null;
+    
         public CalendarTests(AutomationType automationType, TestApplicationType appType)
             : base(automationType, appType)
         {
@@ -20,18 +22,29 @@ namespace FlaUI.Core.UITests.Elements
         [OneTimeSetUp]
         public void SelectDateTest()
         {
-            Console.WriteLine("AutomationType: " + AutomationType.ToString());
+            //Console.WriteLine("AutomationType: " + AutomationType.ToString());
             RestartApplication();
             var mainWindow = Application.GetMainWindow(Automation);
             var tab = mainWindow.FindFirstDescendant(cf => cf.ByControlType(ControlType.Tab)).AsTab();
             tab.SelectTabItem(2);
             //Wait.UntilInputIsProcessed();
-            var calendar = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("calendar")).AsCalendar();
+            calendar = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("calendar")).AsCalendar();
             DateTime date = new DateTime(2020, 5, 21); // 21-May-2020
             calendar.SelectDate(date); 
             DateTime[] selectedDates = calendar.SelectedDates;
             Assert.That(selectedDates, Has.Length.EqualTo(1));
             Assert.That(selectedDates[0], Is.EqualTo(date));
+        }
+        
+        [Test]
+        public void AddToSelectionTest()
+        {
+            DateTime date = new DateTime(2020, 5, 23); // 23-May-2020
+            calendar.AddToSelection(date);
+            DateTime[] selectedDates = calendar.SelectedDates;
+            Assert.That(selectedDates, Has.Length.EqualTo(2));
+            Assert.That(selectedDates[0], Is.EqualTo(new DateTime(2020, 5, 21)));
+            Assert.That(selectedDates[1], Is.EqualTo(date));
         }
     }
 }
