8761ef4426554bfda6d2c8bdd4cd64e54d67d526
ddeltasolutions <daniel.mocanu072180@gmail.com>
2020-01-29 07:36:06 +0200

    Add constants for Win32 Calendar

--- src/FlaUI.Core/WindowsAPI/Constants.cs
+++ src/FlaUI.Core/WindowsAPI/Constants.cs
@@ -2383,5 +2383,79 @@ namespace FlaUI.Core.WindowsAPI
         public static IntPtr TRUE = new IntPtr(1);
         public static IntPtr FALSE = new IntPtr(0);
     }
+    
+    internal class Win32CalendarMessages
+    {
+        public static uint MCM_GETCURSEL = 0x1001;
+        public static uint MCM_SETCURSEL = 0x1002;
+        public static uint MCM_GETSELRANGE = 0x1005;
+        public static uint MCM_SETSELRANGE = 0x1006;
+    }
+    
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SYSTEMTIME 
+    {
+        [MarshalAs(UnmanagedType.U2)] public short Year;
+        [MarshalAs(UnmanagedType.U2)] public short Month;
+        [MarshalAs(UnmanagedType.U2)] public short DayOfWeek;
+        [MarshalAs(UnmanagedType.U2)] public short Day;
+        [MarshalAs(UnmanagedType.U2)] public short Hour;
+        [MarshalAs(UnmanagedType.U2)] public short Minute;
+        [MarshalAs(UnmanagedType.U2)] public short Second;
+        [MarshalAs(UnmanagedType.U2)] public short Milliseconds;
+    }
+    
+    [Flags]
+    public enum ProcessAccessFlags : uint
+    {
+        All = 0x001F0FFF,
+        Terminate = 0x00000001,
+        CreateThread = 0x00000002,
+        VirtualMemoryOperation = 0x00000008,
+        VirtualMemoryRead = 0x00000010,
+        VirtualMemoryWrite = 0x00000020,
+        DuplicateHandle = 0x00000040,
+        CreateProcess = 0x000000080,
+        SetQuota = 0x00000100,
+        SetInformation = 0x00000200,
+        QueryInformation = 0x00000400,
+        QueryLimitedInformation = 0x00001000,
+        Synchronize = 0x00100000
+    }
+    
+    [Flags]
+    public enum AllocationType
+    {
+        Commit = 0x1000,
+        Reserve = 0x2000,
+        Decommit = 0x4000,
+        Release = 0x8000,
+        Reset = 0x80000,
+        Physical = 0x400000,
+        TopDown = 0x100000,
+        WriteWatch = 0x200000,
+        LargePages = 0x20000000
+    }
+    
+    [Flags]
+    public enum MemoryProtection
+    {
+        Execute = 0x10,
+        ExecuteRead = 0x20,
+        ExecuteReadWrite = 0x40,
+        ExecuteWriteCopy = 0x80,
+        NoAccess = 0x01,
+        ReadOnly = 0x02,
+        ReadWrite = 0x04,
+        WriteCopy = 0x08,
+        GuardModifierflag = 0x100,
+        NoCacheModifierflag = 0x200,
+        WriteCombineModifierflag = 0x400
+    }
+    
+    internal class Win32CalendarStyles
+    {
+        public static int MCS_MULTISELECT = 2;
+    }
 }
 #pragma warning restore
