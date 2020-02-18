240d543381347b081f134cfb2a120514f28ec573
ddeltasolutions <daniel.mocanu072180@gmail.com>
2020-02-18 13:23:33 +0200

    for Windows Forms test only reading the date

--- src/FlaUI.Core.UITests/Elements/DateTimePickerTests.cs
+++ src/FlaUI.Core.UITests/Elements/DateTimePickerTests.cs
@@ -24,24 +24,26 @@ namespace FlaUI.Core.UITests.Elements
         {
             //RestartApp();
             var mainWindow = Application.GetMainWindow(Automation);
-            DateTimePicker dateTimePicker = null;
             
             if (ApplicationType == TestApplicationType.Wpf)
             {
                 var tab = mainWindow.FindFirstDescendant(cf => cf.ByControlType(ControlType.Tab)).AsTab();
                 tab.SelectTabItem(2);
                 //Wait.UntilInputIsProcessed();
-                dateTimePicker = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("datePicker")).AsDateTimePicker();
+                var dateTimePicker = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("datePicker")).AsDateTimePicker();
+                
+                DateTime date = new DateTime(2020, 5, 21); // 21-May-2020
+                dateTimePicker.SelectedDate = date;
+                DateTime selectedDate = dateTimePicker.SelectedDate.Value;
+                Assert.That(selectedDate, Is.EqualTo(date));
             }
             else // TestApplicationType.WinForms
             {
-                dateTimePicker = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("dateTimePicker1")).AsDateTimePicker();
+                var dateTimePicker = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("dateTimePicker1")).AsDateTimePicker();
+                
+                DateTime selectedDate = dateTimePicker.SelectedDate.Value;
+                Assert.That(selectedDate, Is.EqualTo(DateTime.Today));
             }
-            
-            DateTime date = new DateTime(2020, 5, 21); // 21-May-2020
-            dateTimePicker.SelectedDate = date;
-            DateTime selectedDate = dateTimePicker.SelectedDate.Value;
-            Assert.That(selectedDate, Is.EqualTo(date));
         }
     }
 }
