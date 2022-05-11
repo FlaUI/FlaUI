b1e7300641589551071422509e487febda3bfa06
Jerome Mohanan <jero2rome@gmail.com>
2022-05-11 14:09:37 +0200

    Update Label tests to use fluent assertions

--- src/FlaUI.Core.UITests/Elements/LabelTests.cs
+++ src/FlaUI.Core.UITests/Elements/LabelTests.cs
@@ -1,5 +1,6 @@
 ﻿using FlaUI.Core.AutomationElements;
 using FlaUI.Core.UITests.TestFramework;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace FlaUI.Core.UITests.Elements
@@ -20,8 +21,8 @@ namespace FlaUI.Core.UITests.Elements
         {
             var window = Application.GetMainWindow(Automation);
             var label = window.FindFirstDescendant(cf => cf.ByText("Test Label")).AsLabel();
-            Assert.That(label, Is.Not.Null);
-            Assert.That(label.Text, Is.EqualTo("Test Label"));
+            label.Should().NotBeNull();
+            label.Text.Should().Be("Test Label");
         }
     }
 }
