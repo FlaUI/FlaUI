0a89fc865f6c87bb138657f9fd1a92950ab4a42f
Jerome Mohanan <jero2rome@gmail.com>
2022-05-11 15:28:39 +0200

    Update Tab tests to use fluent assertions

--- src/FlaUI.Core.UITests/Elements/TabTests.cs
+++ src/FlaUI.Core.UITests/Elements/TabTests.cs
@@ -2,6 +2,7 @@
 using FlaUI.Core.Definitions;
 using FlaUI.Core.Input;
 using FlaUI.Core.UITests.TestFramework;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace FlaUI.Core.UITests.Elements
@@ -25,19 +26,19 @@ namespace FlaUI.Core.UITests.Elements
             var tab = mainWindow.FindFirstDescendant(cf => cf.ByControlType(ControlType.Tab)).AsTab();
             if (ApplicationType == TestApplicationType.Wpf)
             {
-                Assert.That(tab.TabItems, Has.Length.EqualTo(3));
+                tab.TabItems.Should().HaveCount(3);
             }
             else
             {
-                Assert.That(tab.TabItems, Has.Length.EqualTo(2));
+              tab.TabItems.Should().HaveCount(2);
             }
-            Assert.That(tab.SelectedTabItemIndex, Is.EqualTo(0));
+            tab.SelectedTabItemIndex.Should().Be(0);
             tab.SelectTabItem(1);
             Wait.UntilInputIsProcessed();
-            Assert.That(tab.SelectedTabItemIndex, Is.EqualTo(1));
+            tab.SelectedTabItemIndex.Should().Be(1);
             tab.SelectTabItem(0);
             Wait.UntilInputIsProcessed();
-            Assert.That(tab.SelectedTabItemIndex, Is.EqualTo(0));
+            tab.SelectedTabItemIndex.Should().Be(0);
         }
     }
 }
