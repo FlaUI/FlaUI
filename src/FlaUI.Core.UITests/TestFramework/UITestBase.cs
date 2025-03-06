e28b8b17a15b906a286ff28c4dbde8535b699808
Louis Lefevre <louislefev@gmail.com>
2025-03-06 02:06:11 +0000

    Fix path again
    I'm so dumb

--- src/FlaUI.Core.UITests/TestFramework/UITestBase.cs
+++ src/FlaUI.Core.UITests/TestFramework/UITestBase.cs
@@ -35,7 +35,7 @@ namespace FlaUI.Core.UITests.TestFramework
             switch (ApplicationType)
             {
                 case TestApplicationType.WinForms:
-                    app = Application.Launch(Path.Combine(basePath, @"WinFormsApplication\bin\net48\WinFormsApplication.exe"));
+                    app = Application.Launch(Path.Combine(basePath, @"WinFormsApplication\bin\net8.0-windows\WinFormsApplication.exe"));
                     break;
                 case TestApplicationType.Wpf:
                     app = Application.Launch(Path.Combine(basePath, @"WpfApplication\bin\net8.0-windows\WpfApplication.exe"));
