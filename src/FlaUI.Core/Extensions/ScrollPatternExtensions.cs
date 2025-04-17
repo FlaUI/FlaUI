135bcd291814f586f0c994925bebe06ebae55d66
Louis Lefevre <louislefev@gmail.com>
2025-04-17 15:36:26 +0100

    Move methods to ScrollPatternExtensions.cs

---
+++ src/FlaUI.Core/Extensions/ScrollPatternExtensions.cs
@@ -0,0 +1,51 @@
+﻿using FlaUI.Core.Definitions;
+using FlaUI.Core.Patterns;
+
+namespace FlaUI.Core.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="IScrollPattern"/> objects.
+    /// </summary>
+    public static class ScrollPatternExtensions
+    {
+        /// <summary>
+        /// Scrolls horizontally by the specified amount.
+        /// </summary>
+        /// <param name="pattern">Scroll pattern object to act upon.</param>
+        /// <param name="horizontalAmount">Amount to scroll horizontally.</param>
+        public static void ScrollHorizontally(this IScrollPattern pattern, ScrollAmount horizontalAmount)
+        {
+            pattern.Scroll(horizontalAmount, ScrollAmount.NoAmount);
+        }
+
+        /// <summary>
+        /// Scrolls vertically by the specified amount.
+        /// </summary>
+        /// <param name="pattern">Scroll pattern object to act upon.</param>
+        /// <param name="verticalAmount">Amount to scroll vertically.</param>
+        public static void ScrollVertically(this IScrollPattern pattern, ScrollAmount verticalAmount)
+        {
+            pattern.Scroll(ScrollAmount.NoAmount, verticalAmount);
+        }
+
+        /// <summary>
+        /// Scrolls horizontally by the specified percentage.
+        /// </summary>
+        /// <param name="pattern">Scroll pattern object to act upon.</param>
+        /// <param name="horizontalPercent">Percentage to scroll horizontally.</param>
+        public static void SetHorizontalScrollPercent(this IScrollPattern pattern, double horizontalPercent)
+        {
+            pattern.SetScrollPercent(horizontalPercent, ScrollPatternConstants.NoScroll);
+        }
+
+        /// <summary>
+        /// Scrolls vertically by the specified percentage.
+        /// </summary>
+        /// <param name="pattern">Scroll pattern object to act upon.</param>
+        /// <param name="verticalPercent">Percentage to scroll vertically.</param>
+        public static void SetVerticalScrollPercent(this IScrollPattern pattern, double verticalPercent)
+        {
+            pattern.SetScrollPercent(ScrollPatternConstants.NoScroll, verticalPercent);
+        }
+    }
+}
--- src/FlaUI.Core/Patterns/ScrollPattern.cs
+++ src/FlaUI.Core/Patterns/ScrollPattern.cs
@@ -22,12 +22,6 @@ namespace FlaUI.Core.Patterns
 
         void Scroll(ScrollAmount horizontalAmount, ScrollAmount verticalAmount);
         void SetScrollPercent(double horizontalPercent, double verticalPercent);
-
-        void ScrollHorizontally(ScrollAmount horizontalAmount);
-        void SetHorizontalScrollPercent(double horizontalPercent);
-
-        void ScrollVertically(ScrollAmount verticalAmount);
-        void SetVerticalScrollPercent(double verticalPercent);
     }
 
     public interface IScrollPatternPropertyIds
@@ -65,25 +59,5 @@ namespace FlaUI.Core.Patterns
 
         public abstract void Scroll(ScrollAmount horizontalAmount, ScrollAmount verticalAmount);
         public abstract void SetScrollPercent(double horizontalPercent, double verticalPercent);
-
-        public void ScrollHorizontally(ScrollAmount horizontalAmount)
-        {
-            Scroll(horizontalAmount, ScrollAmount.NoAmount);
-        }
-
-        public void SetHorizontalScrollPercent(double horizontalPercent)
-        {
-            SetScrollPercent(horizontalPercent, ScrollPatternConstants.NoScroll);
-        }
-
-        public void ScrollVertically(ScrollAmount verticalAmount)
-        {
-            Scroll(ScrollAmount.NoAmount, verticalAmount);
-        }
-
-        public void SetVerticalScrollPercent(double verticalPercent)
-        {
-            SetScrollPercent(ScrollPatternConstants.NoScroll, verticalPercent);
-        }
     }
 }
